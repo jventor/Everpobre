fbeaf5f1334d89c6235c02addaccbdb29ab7f4d0
Jaime Ventor <jventor@officepoint.es>
2018-04-02 23:34:48 +0100

    Initial project

---
+++ Everpobre/UIColor+theme.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+theme.swift
+//  Everpobre
+//
+//  Created by Jaime Ventor on 2/4/18.
+//  Copyright © 2018 Jaime Ventor. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  static let myGreen = UIColor(red: 115/255, green: 250/255, blue: 121/255, alpha: 1)
+  static let lightGray = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
+
+}
